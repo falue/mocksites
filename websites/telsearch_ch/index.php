5c23cac5a583b7d6d07f51c0b97fc3c997ac6766
fabian lüscher <info@fluescher.ch>
2016-09-26 15:52:40 +0200

    added website telsearch.ch

---
+++ websites/telsearch_ch/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<head>
+<title>Telefonbuch 2.0</title>
+<link rel="stylesheet" type="text/css" href="tools/stylesheet.css" />
+<script src="tools/javascript.js"></script>
+
+<!-- ISO-8859-1 -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<!-- favicon -->
+<!-- <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" /> -->
+
+</head>
+<body>
+  <header>
+    
+    <div class="logo" onclick="window.location.href='index.php';"><img src="tools/logo.png" alt=""> Telefonsuche</div>
+    <a href="#">Telefonbuch</a>
+    <a href="#">Wetter</a>
+    <a href="#">Karte / Route</a>
+    <a href="#">Fahrplan</a>
+    <a href="#">TV</a>
+    <a href="#">Kino</a>
+    
+    
+    <div class="search">
+      <div class="result_label">
+        <div class="result_count" id="result_count">3'718'941 EINTR&Auml;GE</div>
+        <div class="result_bar" id="result_bar">&nbsp;</div>
+      </div>
+      <form action="index.php" method="GET">
+      <input type="text" id="wer" name="wer" placeholder="Wer/Was/Telefonnummer" onkeyup="count(865788);" value="<? echo $_GET["wer"]; ?>" autocomplete="off">
+      <input type="text" id="wo" name="wo" placeholder="Wo" onkeyup="count(26121);" value="<? echo $_GET["wo"]; ?>" autocomplete="off">
+      <label><input type="checkbox" checked="checked"> Privat</label>
+      <label><input type="checkbox" checked="checked"> Firma</label>
+      <input type="submit" id="button_search" value="Suchen">
+      </form>
+      <div class="erweitert">Erweiterte Suche <img src="tools/down.png" alt=""></div>
+    </div>
+  </header>
+  
+
+
+
+  <?php
+  if(isset($_GET["wer"])) {   
+    echo "<article id=\"results\" class=\"results\">
+    1 EINTRAG F&Uuml;R '".strtoupper($_GET["wer"])."' IN '".strtoupper($_GET["wo"])."':
+    <hr>
+    <div class=\"result\">
+      <img src=\"tools/person.png\" alt=\"a\" class=\"result_img\">
+      <h1>Galli, Nina</h1>
+      Chaumontweg 34<br>
+      3030 Bern<br>
+      <h1>XXX XXX XX XX*</h1>
+      <div class=\"details\"><input type=\"button\" value=\"Details\"></div>
+    </div>
+
+    <div class=\"korr\">
+      Sie k&ouml;nnen einen bestehenden <a href=\"#\">Eintrag korrigieren</a>, einen <a href=\"#\">Privateintrag erfassen</a> oder <a href=\"#\">Firmen/Beh&ouml;rdeneintrag erfassen</a>.
+    </div>
+    </article>
+    <div class=\"map\">&nbsp;</div>";
+  } else {
+    echo "<img src=\"tools/top.png\" alt=\"\" class=\"results\">";
+  }
+?>  
+
+
+
+
+</body>
+</html>
---
+++ websites/telsearch_ch/tools/javascript.js
@@ -0,0 +1,33 @@
+function count(counter) {
+	var was = document.getElementById('result_count').innerHTML.replace(/'/g, "");
+	now = was.match(/\d+/);
+	now = now - counter;
+	
+	if (now < 260000 && now > 120000) {
+		document.getElementById('result_bar').style.width = "5%";
+	} else if (now < 120000 && now > 50000) {
+		document.getElementById('result_bar').style.width = "17%";
+	} else if (now < 50000 && now > 1000) {
+		document.getElementById('result_bar').style.width = "33%";
+	} else if (now < 1000) {
+		document.getElementById('result_bar').style.width = "100%";
+	}
+	
+	if (now <= 0) {
+		document.getElementById('result_count').innerHTML = "1 EINTRAG";
+	} else {
+		document.getElementById('result_count').innerHTML = now.formatMoney(0,"'",".") + " EINTR&Auml;GE";
+	}
+}
+
+
+Number.prototype.formatMoney = function(decPlaces, thouSeparator, decSeparator) {
+    var n = this,
+        decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
+        decSeparator = decSeparator == undefined ? "." : decSeparator,
+        thouSeparator = thouSeparator == undefined ? "," : thouSeparator,
+        sign = n < 0 ? "-" : "",
+        i = parseInt(n = Math.abs(+n || 0).toFixed(decPlaces)) + "",
+        j = (j = i.length) > 3 ? j % 3 : 0;
+    return sign + (j ? i.substr(0, j) + thouSeparator : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thouSeparator) + (decPlaces ? decSeparator + Math.abs(n - i).toFixed(decPlaces).slice(2) : "");
+};
