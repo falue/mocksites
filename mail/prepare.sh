b64cce31e05f789613c558961e79c44e92d59a27
Ben Hagen <c.ben.hagen@gmail.com>
2016-10-10 18:04:30 +0200

    Add mailserver

---
+++ mail/prepare.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+mkdir -p config
+touch config/postfix-accounts.cf
+docker run --rm \
+  -e MAIL_USER=jonaswid_92@gmail.com \
+  -e MAIL_PASS=1234 \
+  -ti tvial/docker-mailserver:latest \
+  /bin/sh -c 'echo "$MAIL_USER|$(doveadm pw -s SHA512-CRYPT -u $MAIL_USER -p $MAIL_PASS)"' > config/postfix-accounts.cf
+
+docker run --rm \
+      -v "$(pwd)/config":/tmp/docker-mailserver \
+        -ti tvial/docker-mailserver:latest generate-dkim-config
