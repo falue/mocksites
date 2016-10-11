e0aaf5bea211b730657458d548c3735f32fcf7e3
fabian lüscher <info@fluescher.ch>
2016-10-11 11:13:06 +0200

    urls to local, small design changes

--- websites/lifepulse_ch/index.php
+++ websites/lifepulse_ch/index.php
@@ -14,14 +14,14 @@
 </head>
 
 <body>
-	
+
 <header>
   <div class="nav_top">
     <a href="#">Life Pulse</a>
     <a href="#">Updates</a>
     <a href="#">lifepulse.com</a>
   </div>
-  
+
   <div class="nav">
     <img src="tools/logo.jpg" alt="" class="logo">
     <a href="#">Feed</a> |
@@ -30,17 +30,17 @@
     <a href="#">Fortschritt</a> |
     <a href="#">Community</a> |
     <a href="#">Programme</a>
-    
-    
-    
+
+
+
   </div>
-  
+
   <div class="nav_right">
       <a href="#"><img src="tools/fav.png" alt=""></a>
       <a href="#"><img src="tools/notes.png" alt=""></a>
       <a href="#">Runner92&emsp;<div>&nbsp;</div>&emsp;&emsp;&emsp;&emsp;&emsp;</a>
   </div>
-  
+
   <div class="nav_sub">
     <a href="index.php">Tagebuch</a> |
     <a href="#">Erholungsstatus</a> |
@@ -51,23 +51,28 @@
 
 
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 if(!isset($_GET["s"])) {
   echo "<article id=\"calendar\">";
   /* Set the default timezone */
   date_default_timezone_set("Europe/Zurich");
-  
+
   /* Set the date */
   #$date = strtotime(date("Y-m-d"));
   $date = strtotime(date(file_get_contents("tools/today.txt")));
-  
+
   $day = date('d', $date);
   $month = date('m', $date);
   $year = date('Y', $date);
   $firstDay = mktime(0,0,0,$month, 1, $year);
   $title = strftime('%B', $firstDay);
   $dayOfWeek = date('D', $firstDay);
-  $daysInMonth = cal_days_in_month(0, $month, $year);
-  /* Get the name of the week days */
+	$daysInMonth = date('t', mktime(0,0,0,$month, $year));
+  /*Get the name of the week days */
   $timestamp = strtotime('next Sunday');
   $weekDays = array();
   for ($i = 0; $i < 7; $i++) {
@@ -75,7 +80,7 @@ if(!isset($_GET["s"])) {
       $timestamp = strtotime('+1 day', $timestamp);
   }
   $blank = date('w', strtotime("{$year}-{$month}-01"));
-  
+
   echo "<h1>tagebuch $title $year</h1> &larrb; &rarrb;
   <br><br>
   <table>
@@ -83,13 +88,13 @@ if(!isset($_GET["s"])) {
   foreach($weekDays as $key => $weekDay) {
     echo "<td class=\"text-center\">$weekDay</td>";
   }
-  
+
   echo "</tr><tr>";
-  
+
   for($i = 0; $i < $blank; $i++) {
     echo "<td class=\"noday\"></td>";
   }
-  
+
   for($i = 1; $i <= $daysInMonth; $i++) {
     if($day == $i) {
     echo "<td class=\"today\" onclick=\"upload(0, 0, $date);\" onmouseover=\"show('day_$i');\" onmouseout=\"hide('day_$i');\"><strong>$i</strong><br><br><span id=\"day_$i\" class=\"add\">Daten hochladen</span></td>";
@@ -101,32 +106,32 @@ if(!isset($_GET["s"])) {
       echo "</tr><tr>";
     }
   }
-  
+
   for($i = 0; ($i + $blank + $daysInMonth) % 7 != 0; $i++) {
     echo "<td class=\"noday\"></td>";
   }
-  
+
   echo "</tr>
   </table>
    <br>
   <div class=\"training\">
   <h1>Training Load:</h1><br>
-  
+
   <span>Leicht 0-6 Stunden</span>
   <span>Angemessen 7-12 Stunden</span>
   <span>Fordernd 13-24 Stunden</span>
   <span>Sehr fordernd 25-48 Stunden</span>
   <span>Extrem Mehr als 48 Stunden</span>
   </div>
-   
+
   <div class=\"upload\" id=\"upload\" onclick=\"document.getElementById('upload').style.display = 'none'; document.getElementById('upload_popup').style.display = 'none';\"></div>
   <div class=\"upload_popup\" id=\"upload_popup\"></div>
-  
+
   <br><br>
     <img src=\"tools/footer1.png\" alt=\"\"><br>
     <img src=\"tools/footer2.png\" alt=\"\"><br><br>
   </article>";
- 
+
  } else {
  echo "<article id=\"graph_wrapper\">
  <h1>Uploaded data: ".gmdate("d. m. Y", $_GET["s"])."</h1><br>
@@ -138,27 +143,27 @@ if(!isset($_GET["s"])) {
   <a href=\"javascript:makechart(3);\"><div class=\"button\">".gmdate("d. m. Y", $_GET["s"]-2*24*60*60)."</div></a>
   <a href=\"javascript:makechart(4);\"><div class=\"button\">".gmdate("d. m. Y", $_GET["s"]-3*24*60*60)."</div></a>
   <br>
-  
+
   <div class='wrapper' >
 	<canvas id='graph' class='graph' height=\"400px\" width=\"1600px\"></canvas>
   </div>
   <img src=\"tools/anzeige_pulse.png\" alt=\"\" class=\"border\">
-  
+
   <script src=\"tools/chart.min.js\"></script>
   <script src=\"tools/graph.js\"></script>
-  
+
   <br><br><br>
   <img src=\"tools/footer1.png\" alt=\"\"><br>
   <img src=\"tools/footer2.png\" alt=\"\"><br>
   </article>
-  
+
   <nav id=\"feelings\"><img src=\"tools/feelings.png\" alt=\"\"><div class=\"button\" onclick=\"hide('feelings');\">Jetzt nicht</div><div class=\"button\">Speichern</div></nav>";
 
  }
- 
+
  ?>
- 
- 
+
+
 
 
 
